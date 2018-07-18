bf126686c75492684409127c1afe9b04ad329c24
spotlight <31270657+thienanh96@users.noreply.github.com>
2018-07-18 12:05:52 +0700

    first commit
    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const api = require('./router');
+const express = require('express');
+const path = require('path');
+const port = 3001;
+const cors = require('cors');
+//......
+
+var app = express();
+app.set('view engine', 'ejs');
+app.use(cors());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', api);
+var server = require('http').Server(app);
+server.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running on port ' + port);
+})
+
---
+++ router.js
@@ -0,0 +1,70 @@
+const rp = require('request-promise');
+const request = require('request');
+var express = require('express');
+var router = express.Router();
+var CryptoJS = require('crypto-js');
+
+router.get('/phim', function (req, res, next) {
+    let url = req.query.url;
+    let server = req.query.server;
+    let url2 = url + '.html';
+    let mId = [];
+    let mIdphimbo = url2.match(/e(\d+)\./i);
+    let mIdphimle = [];
+    if(mIdphimbo === null || mIdphimbo === undefined){
+        mIdphimle = url2.match(/(\d+)\./i);
+        mId = mIdphimle;
+    } else {
+        mIdphimbo = url2.match(/(\d+)\_/i);
+        mId = mIdphimbo;
+    }
+    let pass = 'bilutv.com' + '4590481877' + mId[1];
+    request(url, (error, response, body) => {
+        body += '';
+        if (!error) {
+            let arrFile = body.match(/"file":"([^"]+)"/g);
+            let arrServer = body.match(/("server":"\w+")/g);
+            arrServer = arrServer.map(el => {
+                let filter = el.match(/("\w+")/g);
+                filter = filter[1].replace('"', '').replace('"', '').trim();
+                return filter
+            })
+            let arrLink = [];
+            if (arrFile) {
+                for (let file of arrFile) {
+                    let mf = file.match(/"file":"([^"]+)"/i);
+                    if (mf && mf[1]) {
+                        let encFile = mf[1].replace(/\\\//g, '/');
+                        let bytes = CryptoJS.AES.decrypt(encFile, pass);
+                        let plaintext = bytes.toString(CryptoJS.enc.Utf8);
+                        arrLink.push(plaintext);
+                    }
+                }
+            }
+            let returnLink;
+            if (server + '' === 'sb') {
+                let indexServerSb1 = arrServer.indexOf(server);
+                let indexServerSb2 = arrServer.indexOf(server, indexServerSb1 + 1);
+                if (indexServerSb2 !== -1) {
+                    returnLink = arrLink[indexServerSb2]
+                } else {
+                    returnLink = arrLink[indexServerSb1];
+                }
+            } else {
+                let indexServer = arrServer.indexOf(server);
+                if (indexServer !== -1) {
+                    returnLink = arrLink[indexServer];
+                }
+            }
+            return res.render("index.ejs", {
+                src: returnLink
+            });
+        } else {
+            return res.json
+        }
+    })
+
+
+});
+
+module.exports = router
