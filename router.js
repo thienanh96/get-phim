d843ee6b35eb73954e9c5c0b375e5fb8c4cd41cd
spotlight <31270657+thienanh96@users.noreply.github.com>
2018-07-23 12:05:08 +0700

    6
    6

--- router.js
+++ router.js
@@ -76,7 +76,7 @@ router.get('/phim', function (req, res, next) {
                     headers: {
                         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
                     },
-                    body: {
+                    data: {
                         bodyy: urlPM
                     }
                 }, (error, response, bodyy) => {
