d3de2e4713f63a5ac9f0e5839114c33efe3f0dbb
spotlight <31270657+thienanh96@users.noreply.github.com>
2018-07-30 17:12:54 +0700

    4
    4

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const port = 3001;
 const cors = require('cors');
+var model = require('./model');
 //......
 
 var app = express();
