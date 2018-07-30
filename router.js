9ab2bd77e9ecbd04bd4786b7a17fc8084fa4b177
spotlight <31270657+thienanh96@users.noreply.github.com>
2018-07-30 17:11:21 +0700

    4
    4

--- router.js
+++ router.js
@@ -8,35 +8,9 @@ var http = followRedirects.http;
 var https = followRedirects.https;
 var cheerio = require('cheerio');
 var Crawler = require("crawler");
+var Film = require('./model');
+var access_token = 'EAAZA5TUfQPxcBADeYgJY9m45LZCCBzxZBrldWru5VRkrx1RFs7bYiZCZBheZAjFZAAePAvZBLtl86MyNFADoaHlZC6sK30WBgnMqDpLsWllY1NzyEGQ2XiWYkZCP1z9G0xawNATOlAhxwI1cgGQjeNu6lBFddjns6NZBlKCJpQETFpoCZCw8a21VW8fhUWbxfn7IZBZBEZD'
 
-function unpack(p) {
-    c = p;
-    var a = 5,
-        x = 1;
-    while (x < a) {
-        c = unescape(c);
-        if (/eval\(+function\(/.test(c)) {
-            c = depack(c);
-            x++
-        } else {
-            break
-        }
-    };
-    c = unescape(c);
-    return c;
-}
-
-function depack(p) {
-    if (p != "") {
-        c = unescape(p);
-        var _e = eval,
-            s = "eval=function(v){c=v;};" + c + ";eval=_e;";
-        eval(s)
-    } else {
-        c = p
-    };
-    return c
-}
 
 router.get('/phim', function (req, res, next) {
     let domain = req.query.domain;
@@ -44,61 +18,7 @@ router.get('/phim', function (req, res, next) {
     let server = req.query.server;
 
     if (domain === 'phimmoi') {
-        request({
-            method: 'GET',
-            url: url,
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
-            }
-        }, (error, response, body) => {
-            body += '';
-            var $ = cheerio.load(body, {
-                decodeEntities: false
-            });
-            let elsScript = $('script').toArray();
-            let target = '';
-            for (el of elsScript) {
-                if (el.children[0]) {
-                    let data = el.children[0].data;
-                    if (data && data.includes('eval')) {
-                        target = data.slice(1, data.length);
-                        break;
-                    }
-                }
-            }
-            if (target && target !== '') {
-                let decodedText = unpack(target);
-                let urlPM = decodedText.match(/http:\/\/episode.*"/g)[0].split('"')[0];
-                console.log('episode: ', urlPM)
-                request({
-                    method: 'POST',
-                    url: 'http://phim360.xyz/index.php',
-                    headers: {
-                        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
-                    },
-                    json: true,
-                    body: {
-                        'bodyy': urlPM +''
-                    }
-                }, (error, response, bodyy) => {
-                    console.log('RTT" ',bodyy);
-                    bodyy += '';
-                    let match = bodyy.match(/https:\\\/\\\/video.*"/g);
-                    if(match !== null){
-                        let finalUrl = match[0].split('"')[0];
-                        finalUrl = finalUrl.replace(/\\\//g, '/')
-                        return chooseTemplate(finalUrl, res);
-                    } else {
-                        return chooseTemplate('', res);
-                    }
-
-
-                })
-            } else {
-                return chooseTemplate('', res);
-            }
-
-        })
+        return chooseTemplate('', res);
     } else {
         let url2 = url + '.html';
         let mId = [];
@@ -221,5 +141,111 @@ var chooseTemplate = (url, res) => {
         return res.render("index3.ejs");
     }
 }
+
+var getFilmfromFB = (idFilm) => {
+    let options = {
+        method: 'GET',
+        uri: 'https://graph.facebook.com/v3.1/' + idFilm,
+        json: true,
+        headers: {
+            /* 'content-type': 'application/x-www-form-urlencoded' */ // Is set automatically
+            'Content-Type': 'application/json'
+        },
+        qs: {
+            access_token: access_token,
+            fields: 'source'
+        }
+    };
+    return rp(options);
+}
+
+router.get('/getfb', function (req, res, next) {
+    let idFilm = req.query.play + '';
+
+    Film.getFilm(idFilm).then(film => {
+        if (film) {
+            let timeFilm = film.time;
+            if (Date.now() - timeFilm > 1000) {
+                getFilmfromFB(idFilm).then(filmObj => {
+                    if (filmObj.source) {
+                        let newSource = filmObj.source;
+                        Film.updateFilm(idFilm, {
+                            source: newSource,
+                            time: Date.now()
+                        }).then(updatedFilm => {
+                            console.log('update thanh cong: ', updatedFilm);
+                            return res.render('index.ejs', {
+                                src: updatedFilm.source
+                            })
+                        }, err => {
+                            return res.render('index.ejs', {
+                                src: film.source
+                            })
+                        })
+                    }
+                }, err => {
+                    return res.render('index3.ejs', {
+                        src: ''
+                    })
+                })
+            } else {
+                return res.render('index.ejs', {
+                    src: film.source
+                })
+            }
+        } else {
+            return res.render('index3.ejs', {
+                src: ''
+            })
+        }
+    }, err => {
+        console.log('err: ', err);
+    })
+})
+
+router.get('/uploadfilm', function (req, res, next) {
+    let idFilm = req.query.id + '';
+    getFilmfromFB(idFilm).then(film => {
+        if (film.source) {
+            console.log('srt: ',film.source)
+            Film.getFilm(idFilm).then(filmm => {
+                if (!filmm) {
+                    Film.createFilm({
+                        idFilm: idFilm,
+                        source: film.source,
+                        time: Date.now()
+                    }).then(newFilm => res.json({
+                        success: true,
+                        msg: 'Upload phim thanh cong!'
+                    }),err => res.json({
+                        success: false,
+                        msg: 'Upload phim that bai!'
+                    }))
+                } else {
+                    return res.json({
+                        success: false,
+                        msg: 'Phim da ton tai!'
+                    })
+                }
+            },err => res.json({
+                success: false,
+                msg: 'Xay ra loi tim kiem DB!'
+            }))
+        } else {
+            return res.json({
+                success: false,
+                msg: 'Xay ra loi tim kiem Facebook!'
+            })
+        }
+    }, err => {
+        console.log(err);
+        return res.json({
+            success: false,
+            msg: 'Xay ra loi tim kiem Facebook!'
+        })
+    })
+})
+
+
 
 module.exports = router
