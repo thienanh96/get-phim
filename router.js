df4a7bb0738b5fdf2054f6ed39cf5706de857a61
spotlight <31270657+thienanh96@users.noreply.github.com>
2018-08-20 17:55:00 +0700

    6
    6

--- router.js
+++ router.js
@@ -23,6 +23,7 @@ iplocation(ip_info.clientIp, function (error, ress) {
     
 })
 
+
 router.get('/library/adnow-1', function (req, res, next) {
     return res.sendFile('views/adnow-1.js', { root : __dirname})
     
