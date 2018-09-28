c32b60b6e6c9d052afa558c2c326c9a330d03a53
spotlight <31270657+thienanh96@users.noreply.github.com>
2018-09-28 18:53:24 +0700

    4
    4

--- router.js
+++ router.js
@@ -206,7 +206,7 @@ router.get('/getfb', function (req, res, next) {
                 getFilmfromFB(idFilm).then(filmObj => {
                     if (filmObj.source) {
                         let newSource = filmObj.source;
-                        newSource = newSource.replace('video.xx.fbcdn.net','scontent.fhan5-2.fna.fbcdn.net')
+                        newSource = newSource.replace('video.xx.fbcdn.net','video.fhan4-1.fna.fbcdn.net')
                         Film.updateFilm(idFilm, {
                             source: newSource,
                             time: Date.now()
